fea4155e8cc10d6050f85026b7d30debcd64af2a
thovid <theuklid@gmail.com>
2017-09-13 15:55:24 +0200

    logging, cleanup

--- composer/src/main/java/com/rewedigital/examples/msintegration/composer/composing/ValidatingContentFetcher.java
+++ composer/src/main/java/com/rewedigital/examples/msintegration/composer/composing/ValidatingContentFetcher.java
@@ -5,33 +5,42 @@ import static java.util.Objects.requireNonNull;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.damnhandy.uri.template.UriTemplate;
 import com.spotify.apollo.Client;
 import com.spotify.apollo.Request;
 import com.spotify.apollo.Response;
-import com.spotify.apollo.Status;
+
+import okio.ByteString;
 
 public class ValidatingContentFetcher implements ContentFetcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidatingContentFetcher.class);
 
     private final Client client;
     private final Map<String, Object> parsedPathArguments;
 
-    public ValidatingContentFetcher(Client client, Map<String, Object> parsedPathArguments) {
+    public ValidatingContentFetcher(final Client client, final Map<String, Object> parsedPathArguments) {
         this.client = requireNonNull(client);
         this.parsedPathArguments = requireNonNull(parsedPathArguments);
     }
 
-
     @Override
-    public CompletableFuture<Response<String>> fetch(String path) {
+    public CompletableFuture<Response<String>> fetch(final String path) {
         if (path == null || path.trim().isEmpty()) {
-            // LOGGER.warn("Empty path attribute in include found");
-            return CompletableFuture.completedFuture(Response.forStatus(Status.OK).withPayload(""));
+            LOGGER.warn("Empty path attribute in include found");
+            return CompletableFuture.completedFuture(Response.forPayload(""));
         }
 
         final String expandedPath = UriTemplate.fromTemplate(path).expand(parsedPathArguments);
         return client.send(Request.forUri(expandedPath, "GET"))
-            .thenApply(response -> response.withPayload(response.payload().map(p -> p.utf8()).orElse("")))
+            .thenApply(this::toStringPayload)
             .toCompletableFuture();
+    }
+
+    private Response<String> toStringPayload(final Response<ByteString> response) {
+        return response.withPayload(response.payload().map(p -> p.utf8()).orElse(""));
     }
 }
