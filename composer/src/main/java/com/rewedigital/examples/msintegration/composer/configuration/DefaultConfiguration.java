70c41b22c84f32c10a14392baa0cd82650550b49
Thomas Vidic <thomas.vidic@rewe-digital.com>
2017-09-19 13:36:11 +0200

    session enabled config

--- composer/src/main/java/com/rewedigital/examples/msintegration/composer/configuration/DefaultConfiguration.java
+++ composer/src/main/java/com/rewedigital/examples/msintegration/composer/configuration/DefaultConfiguration.java
@@ -8,20 +8,23 @@ import com.typesafe.config.ConfigFactory;
 
 public class DefaultConfiguration {
 
+    private static final Map<String, Object> defaultConfiguration = initDefaultConfigurationMap();
+
     public static Config withDefaults(final Config config) {
         return config.withFallback(defaultConfiguration());
     }
 
     public static Config defaultConfiguration() {
-        return ConfigFactory.parseMap(defaultConfigurationMap(), "default-configuration");
+        return ConfigFactory.parseMap(defaultConfiguration, "default-configuration");
     }
 
-    private static Map<String, Object> defaultConfigurationMap() {
+    private static Map<String, Object> initDefaultConfigurationMap() {
         Map<String, Object> result = new HashMap<>();
         result.put("composer.html.include-tag", "rewe-digital-include");
         result.put("composer.html.content-tag", "rewe-digital-content");
         result.put("composer.html.asset-options-attribute", "data-rd-options");
+
+        result.put("composer.session.enable", Boolean.TRUE);
         return result;
     }
-
 }
