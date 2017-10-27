bbe2a3123896cbe05dcd432b4894f10d331297c5
Thomas Vidic <thomas.vidic@rewe-digital.com>
2017-10-27 11:54:50 +0200

    fixed import

--- product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEvent.java
+++ product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEvent.java
@@ -3,8 +3,7 @@ package com.rewedigital.examples.msintegration.productinformation.product;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 @Entity
 public class ProductEvent {
@@ -20,13 +19,48 @@ public class ProductEvent {
 
     private String payload;
 
-    public String toMessage() {
-        // TODO Auto-generated method stub
-        return null;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
     }
 
     public String getKey() {
         return key;
     }
 
+    public void setKey(final String key) {
+        this.key = key;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(final LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(final String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(final String payload) {
+        this.payload = payload;
+    }
+
+    public String toMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
