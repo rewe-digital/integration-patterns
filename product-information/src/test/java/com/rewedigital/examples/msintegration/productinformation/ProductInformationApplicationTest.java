3dfd14243131afeb37929c9dfa598089017c704b
Dirk Leidiger <Dirk.Leidiger@rewe-digital.com>
2017-10-27 15:16:43 +0200

    ProductInformationApplicationTest fixed.

--- product-information/src/test/java/com/rewedigital/examples/msintegration/productinformation/ProductInformationApplicationTest.java
+++ product-information/src/test/java/com/rewedigital/examples/msintegration/productinformation/ProductInformationApplicationTest.java
@@ -2,12 +2,7 @@ package com.rewedigital.examples.msintegration.productinformation;
 
 import com.rewedigital.examples.msintegration.productinformation.helper.AbstractIntegrationTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ProductInformationApplicationTest extends AbstractIntegrationTest {
 
 	@Test
