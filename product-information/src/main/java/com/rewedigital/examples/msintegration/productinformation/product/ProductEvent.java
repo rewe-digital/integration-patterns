0d0aeec59c0f697e614be34b23739fd0606eefed
Thomas Vidic <thomas.vidic@rewe-digital.com>
2017-10-27 11:27:45 +0200

    work on publisher

--- product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEvent.java
+++ product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEvent.java
@@ -2,4 +2,14 @@ package com.rewedigital.examples.msintegration.productinformation.product;
 
 public class ProductEvent {
 
+    public String toMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getKey() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
--- product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEventPublisher.java
+++ product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ProductEventPublisher.java
@@ -1,16 +1,32 @@
 package com.rewedigital.examples.msintegration.productinformation.product;
 
-import org.springframework.kafka.core.KafkaTemplate;
+import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Component
 public class ProductEventPublisher {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductEventPublisher.class);
 
-    public ProductEventPublisher(final KafkaTemplate<String, String> kafkaTemplate) {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final String topic;
+
+    @Inject
+    public ProductEventPublisher(final KafkaTemplate<String, String> kafkaTemplate,
+        @Value("${productqueue.topic_name}") final String topic) {
         this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
     }
-    
-    public void publish(final ProductEvent productEvent) {
-        
+
+    public ListenableFuture<SendResult<String, String>> publish(final ProductEvent productEvent) {
+        LOGGER.debug("publishing event [%s] to topic [%s]");
+        return kafkaTemplate.send(topic, productEvent.getKey(), productEvent.toMessage());
     }
 }
