c4f21a8f98fc0bb46de3e378f17ca96867ce5de0
Thomas Vidic <thomas.vidic@rewe-digital.com>
2017-10-27 14:29:19 +0200

    handle spring event

--- product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/scheduling/ProductEventProcessingTask.java
+++ product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/scheduling/ProductEventProcessingTask.java
@@ -1,19 +1,22 @@
 package com.rewedigital.examples.msintegration.productinformation.scheduling;
 
-import com.rewedigital.examples.msintegration.productinformation.product.ProductEvent;
-import com.rewedigital.examples.msintegration.productinformation.product.ProductEventPublisher;
-import com.rewedigital.examples.msintegration.productinformation.product.ProductEventRepository;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.inject.Inject;
-import java.util.List;
+import com.rewedigital.examples.msintegration.productinformation.product.ProductEvent;
+import com.rewedigital.examples.msintegration.productinformation.product.ProductEventPublisher;
+import com.rewedigital.examples.msintegration.productinformation.product.ProductEventRepository;
 
 @Component
-public class ProductEventProcessingTask {
+public class ProductEventProcessingTask implements ApplicationListener<ProductEvent.Message> {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductEventProcessingTask.class);
 
@@ -21,9 +24,16 @@ public class ProductEventProcessingTask {
     final private ProductEventRepository productEventRepository;
 
     @Inject
-    public ProductEventProcessingTask(final ProductEventPublisher eventPublisher, final ProductEventRepository productEventRepository) {
-        this.eventPublisher=eventPublisher;
+    public ProductEventProcessingTask(final ProductEventPublisher eventPublisher,
+        final ProductEventRepository productEventRepository) {
+        this.eventPublisher = eventPublisher;
         this.productEventRepository = productEventRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(final ProductEvent.Message event) {
+        final ProductEvent productEvent = productEventRepository.getOne(event.id());
+        publishEventAndDeleteFromDB(productEvent);
     }
 
     @Scheduled(fixedRate = 1000)
@@ -31,7 +41,6 @@ public class ProductEventProcessingTask {
         LOG.debug("performing next message batch");
 
         pickUnprocessedMessages().stream().forEach(e -> publishEventAndDeleteFromDB(e));
-
     }
 
     private List<ProductEvent> pickUnprocessedMessages() {
