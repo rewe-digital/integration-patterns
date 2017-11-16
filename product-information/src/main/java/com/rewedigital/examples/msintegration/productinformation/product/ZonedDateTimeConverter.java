e300609d8d2245cdc602fa06b7b68381a9c4cdfc
Sebastian Gauder <rattakresch@gmail.com>
2017-11-16 15:42:59 +0100

    Add ZonedDateTime converter

---
+++ product-information/src/main/java/com/rewedigital/examples/msintegration/productinformation/product/ZonedDateTimeConverter.java
@@ -0,0 +1,24 @@
+package com.rewedigital.examples.msintegration.productinformation.product;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Converter(autoApply = true)
+public class ZonedDateTimeConverter implements AttributeConverter<ZonedDateTime, Timestamp> {
+
+    @Override
+    public java.sql.Timestamp convertToDatabaseColumn(ZonedDateTime entityValue) {
+       return Timestamp.from(entityValue.toInstant());
+    }
+
+    @Override
+    public ZonedDateTime convertToEntityAttribute(java.sql.Timestamp databaseValue) {
+        LocalDateTime localDateTime = databaseValue.toLocalDateTime();
+        return localDateTime.atZone(ZoneId.systemDefault());
+    }
+
+}
