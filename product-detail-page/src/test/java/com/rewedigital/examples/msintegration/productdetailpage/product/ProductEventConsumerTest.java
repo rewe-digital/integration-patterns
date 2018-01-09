5e6da993c409dce7c2395619125aaac2f0b40d9c
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-01-09 12:57:39 +0100

    test improvements: better mockito usage, removed comments and improved test names

--- product-detail-page/src/test/java/com/rewedigital/examples/msintegration/productdetailpage/product/ProductEventConsumerTest.java
+++ product-detail-page/src/test/java/com/rewedigital/examples/msintegration/productdetailpage/product/ProductEventConsumerTest.java
@@ -1,9 +1,16 @@
 package com.rewedigital.examples.msintegration.productdetailpage.product;
 
-import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.configuration.SimpleConsumerTopicConfig;
-import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState;
-import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.exception.TemporaryKafkaProcessingError;
-import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.unprocessable.UnprocessableEventService;
+import static com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState.SUCCESS;
+import static com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState.TEMPORARY_ERROR;
+import static com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState.UNEXPECTED_ERROR;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +18,10 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.kafka.support.Acknowledgment;
 
-import java.util.UUID;
-
-import static com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState.*;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.*;
+import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.EventProcessingState;
+import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.configuration.SimpleConsumerTopicConfig;
+import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.exception.TemporaryKafkaProcessingError;
+import com.rewedigital.examples.msintegration.productdetailpage.infrastructure.eventing.unprocessable.UnprocessableEventService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductEventConsumerTest {
@@ -41,38 +47,38 @@ public class ProductEventConsumerTest {
     }
 
     @Test
-    public void testEmptyPayload() {//Event syntax Error -> should neither be processed nor stored as unprocessable
+    public void eventWithSyntaxErrorShouldNeitherBeProcessedNorStoredAsUnprocessable() {
         productEventConsumer = new ProductEventConsumer(mockedProcessor(SUCCESS), unprocessableEventService);
         productEventConsumer.listen(CONSUMER_RECORD,ack);
 
         verify(ack).acknowledge();
-        verify(unprocessableEventService, times(0)).save(anyObject());
+        verify(unprocessableEventService, never()).save(anyObject());
     }
 
     @Test
-    public void testUnexpectedError() {//Unexpected Error -> Store as unprocessable
-        ProductEventProcessor processor = mockedProcessor(UNEXPECTED_ERROR);
+    public void eventLeadingToUnexpectedErrorShouldBeStoredAsUnprocessable() {
+        final ProductEventProcessor processor = mockedProcessor(UNEXPECTED_ERROR);
         productEventConsumer = new ProductEventConsumer(processor, unprocessableEventService);
         productEventConsumer.listen(CONSUMER_RECORD,ack);
 
         verify(ack).acknowledge();
-        verify(processor, times(1)).processConsumerRecord(anyObject());
-        verify(unprocessableEventService, times(1)).save(anyObject());
+        verify(processor).processConsumerRecord(anyObject());
+        verify(unprocessableEventService).save(anyObject());
     }
 
     @Test(expected = TemporaryKafkaProcessingError.class)
-    public void testTemporaryError() {//Kafka processing Error -> Store as unprocessable
-        ProductEventProcessor processor = mockedProcessor(TEMPORARY_ERROR);
+    public void temporaryErrorShouldStoreEventAsUnprocessable() {
+        final ProductEventProcessor processor = mockedProcessor(TEMPORARY_ERROR);
         productEventConsumer = new ProductEventConsumer(processor, unprocessableEventService);
         productEventConsumer.listen(CONSUMER_RECORD,ack);
 
-        verify(ack, times(0)).acknowledge();
-        verify(processor, times(1)).processMessage(anyObject());
-        verify(unprocessableEventService, times(1)).save(anyObject());
+        verify(ack, never()).acknowledge();
+        verify(processor).processMessage(anyObject());
+        verify(unprocessableEventService).save(anyObject());
     }
 
-    private ProductEventProcessor mockedProcessor(EventProcessingState expectedEventProcessingState) {
-        ProductEventProcessor productEventProcessor = mock(ProductEventProcessor.class);
+    private ProductEventProcessor mockedProcessor(final EventProcessingState expectedEventProcessingState) {
+        final ProductEventProcessor productEventProcessor = mock(ProductEventProcessor.class);
         when(productEventProcessor.getTopicConfig()).thenReturn(new SimpleConsumerTopicConfig());
         when(productEventProcessor.processConsumerRecord(CONSUMER_RECORD)).thenReturn(expectedEventProcessingState);
         return productEventProcessor;
