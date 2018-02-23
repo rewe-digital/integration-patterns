9ccf626728be26b769ed5b6d786bb0607e246414
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-02-23 13:14:54 +0100

    extracted SessionData to hold low level session management stuff

--- composer/src/main/java/com/rewedigital/examples/msintegration/composer/session/Session.java
+++ composer/src/main/java/com/rewedigital/examples/msintegration/composer/session/Session.java
@@ -5,10 +5,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import com.spotify.apollo.Response;
 
@@ -19,12 +16,15 @@ public class Session {
         <T> Response<T> writeTo(final Response<T> response, final Map<String, String> sessionData, boolean dirty);
     }
 
-    private static final String sessionPrefix = "x-rd-";
     private static final Session emptySession = new Session(new HashMap<>());
 
-    private final Map<String, String> data;
+    final SessionData data;
 
     private Session(final Map<String, String> data) {
+        this(new SessionData(data));
+    }
+
+    private Session(final SessionData data) {
         this.data = data;
     }
 
@@ -35,37 +35,17 @@ public class Session {
     public static <T> Session of(final Response<T> response) {
         final List<Map.Entry<String, String>> data = response.headerEntries()
             .stream()
-            .filter(Session::isSessionEntry)
+            .filter(SessionData::isSessionEntry)
             .collect(toList());
         return new Session(toMap(data));
     }
 
     public Optional<String> get(final String key) {
-        final String lookupKey = prefixed(key);
-        // not using Map.get here due to equalsIgnoreCase
-        for (final Map.Entry<String, String> entry : data.entrySet()) {
-            if (entry.getKey().equalsIgnoreCase(lookupKey)) {
-                return Optional.ofNullable(entry.getValue());
-            }
-        }
-        return Optional.empty();
+        return data.get(key);
     }
 
     public Session withValuesMergedFrom(final Session other) {
-        final Map<String, String> mergedData = merge(this.data, other.data);
-        return new Session(mergedData);
-    }
-
-    Map<String, String> rawData() {
-        return new HashMap<>(data);
-    }
-
-    private static String prefixed(final String key) {
-        return sessionPrefix + key;
-    }
-
-    private static boolean isSessionEntry(final Map.Entry<String, String> entry) {
-        return entry.getKey().toLowerCase().startsWith(sessionPrefix);
+        return new Session(data.mergedWith(other.data));
     }
 
     private static Map<String, String> toMap(final List<Map.Entry<String, String>> entries) {
@@ -75,22 +55,5 @@ public class Session {
             result.put(entry.getKey(), entry.getValue());
         }
         return result;
-    }
-
-    private static Map<String, String> merge(final Map<String, String> first, final Map<String, String> second) {
-        final Map<String, String> newData = new HashMap<String, String>(first);
-        newData.putAll(second);
-        // prune empty values
-        return newData.entrySet().stream()
-            .filter(Session::nonEmptyValue)
-            .collect(entryCollector());
-    }
-
-    private static boolean nonEmptyValue(final Map.Entry<String, String> entry) {
-        return entry.getValue() != null && !"".equals(entry.getValue().trim());
-    }
-
-    private static Collector<Entry<String, String>, ?, Map<String, String>> entryCollector() {
-        return Collectors.toMap(e -> e.getKey(), e -> e.getValue());
     }
 }
--- composer/src/main/java/com/rewedigital/examples/msintegration/composer/session/SessionRoot.java
+++ composer/src/main/java/com/rewedigital/examples/msintegration/composer/session/SessionRoot.java
@@ -2,10 +2,7 @@ package com.rewedigital.examples.msintegration.composer.session;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import com.spotify.apollo.Request;
 import com.spotify.apollo.Response;
@@ -18,14 +15,16 @@ public class SessionRoot {
     }
 
     private static final String sessionIdKey = "session-id";
-    private static final String sessionPrefix = "x-rd-";
-
     private static final SessionRoot emptySession = new SessionRoot(new HashMap<>(), false);
 
-    private final Map<String, String> data;
+    private final SessionData data;
     private final boolean dirty;
 
     private SessionRoot(final Map<String, String> data, final boolean dirty) {
+        this(new SessionData(data), dirty);
+    }
+
+    private SessionRoot(final SessionData data, final boolean dirty) {
         this.data = data;
         this.dirty = dirty;
     }
@@ -39,7 +38,7 @@ public class SessionRoot {
     }
 
     public static SessionRoot of(final Map<String, String> data, final boolean dirty) {
-        return new SessionRoot(new HashMap<>(data), dirty);
+        return new SessionRoot(data, dirty);
     }
 
     public Request enrich(final Request request) {
@@ -47,14 +46,7 @@ public class SessionRoot {
     }
 
     public Optional<String> get(final String key) {
-        final String lookupKey = prefixed(key);
-        // not using Map.get here due to equalsIgnoreCase
-        for (final Map.Entry<String, String> entry : data.entrySet()) {
-            if (entry.getKey().equalsIgnoreCase(lookupKey)) {
-                return Optional.ofNullable(entry.getValue());
-            }
-        }
-        return Optional.empty();
+        return data.get(key);
     }
 
     public Optional<String> getId() {
@@ -66,17 +58,14 @@ public class SessionRoot {
     }
 
     public SessionRoot withValuesMergedFrom(final Session other) {
-        final Map<String, String> mergedData = merge(this.data, other.rawData());
-        // retain original session id if present
-        getId().ifPresent(i -> mergedData.put(prefixed(sessionIdKey), i));
-        final boolean newDirty = !data.equals(mergedData);
-        return new SessionRoot(mergedData, newDirty || dirty);
+        final SessionData mergedData = data.mergedWith(other.data);
+        final SessionData newData = getId().map(id -> mergedData.with(sessionIdKey, id)).orElse(mergedData);
+        final boolean newDirty = !data.equals(newData);
+        return new SessionRoot(newData, newDirty || dirty);
     }
 
     public SessionRoot withId(final String sessionId) {
-        final Map<String, String> sessionData = new HashMap<>(data);
-        sessionData.put(prefixed(sessionIdKey), sessionId);
-        return new SessionRoot(sessionData, true);
+        return new SessionRoot(data.with(sessionIdKey, sessionId), true);
     }
 
     public boolean isDirty() {
@@ -84,37 +73,10 @@ public class SessionRoot {
     }
 
     public Map<String, String> rawData() {
-        return new HashMap<>(data);
+        return data.rawData();
     }
 
     private Map<String, String> asHeaders() {
-        return data.entrySet().stream()
-            .filter(SessionRoot::isSessionEntry)
-            .collect(entryCollector());
-    }
-
-    private static String prefixed(final String key) {
-        return sessionPrefix + key;
-    }
-
-    private static boolean isSessionEntry(final Map.Entry<String, String> entry) {
-        return entry.getKey().toLowerCase().startsWith(sessionPrefix);
-    }
-
-    private static Map<String, String> merge(final Map<String, String> first, final Map<String, String> second) {
-        final Map<String, String> newData = new HashMap<String, String>(first);
-        newData.putAll(second);
-        // prune empty values
-        return newData.entrySet().stream()
-            .filter(SessionRoot::nonEmptyValue)
-            .collect(entryCollector());
-    }
-
-    private static boolean nonEmptyValue(final Map.Entry<String, String> entry) {
-        return entry.getValue() != null && !"".equals(entry.getValue().trim());
-    }
-
-    private static Collector<Entry<String, String>, ?, Map<String, String>> entryCollector() {
-        return Collectors.toMap(e -> e.getKey(), e -> e.getValue());
+        return data.asHeaders();
     }
 }
