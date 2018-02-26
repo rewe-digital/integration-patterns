664fe66b598a34a2efaea1594b4579d58412bdaf
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-02-26 17:11:18 +0100

    added proejct specific configuration for composer

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ nohup java -jar ./header-footer/target/header-footer-0.0.1-SNAPSHOT.jar &> heade
 echo "Starting product detail page."
 nohup java -jar ./product-detail-page/target/product-detail-page-0.0.1-SNAPSHOT.jar  &> product-detail.out&
 echo "Starting composer."
-nohup java -jar ./composer/target/composer.jar &> composer.out&
+nohup java -jar ./composer/target/composer.jar --config ./composer-config/composer.conf &> composer.out&
 echo "Starting product-information."
 nohup java -jar ./product-information/target/product-information-0.0.1-SNAPSHOT.jar &> product-information.out&
 echo "Starting simple kafka broker."
