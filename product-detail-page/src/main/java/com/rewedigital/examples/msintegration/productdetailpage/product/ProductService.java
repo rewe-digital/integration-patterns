8189c94b46f59abca48771a13617c5011ecf01ed
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-03-28 22:57:17 +0200

    added default products, added config for header-footer host

--- product-detail-page/src/main/java/com/rewedigital/examples/msintegration/productdetailpage/product/ProductService.java
+++ product-detail-page/src/main/java/com/rewedigital/examples/msintegration/productdetailpage/product/ProductService.java
@@ -1,16 +1,13 @@
 package com.rewedigital.examples.msintegration.productdetailpage.product;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private static final Logger LOG = LoggerFactory.getLogger(ProductService.class);
 
-    private final JpaProductRepository repository;
+	private final JpaProductRepository repository;
 
     @Inject
     public ProductService(JpaProductRepository repository) {
