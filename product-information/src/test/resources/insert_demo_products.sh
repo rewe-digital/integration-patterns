cccd775e7a04337d71d4a822c95a73dd8273b25b
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-05-09 11:14:19 +0200

    updated port mapping so that docker and local version can run in parallel, deleted simplekafka, as now docker based kafka can be used

--- product-information/src/test/resources/insert_demo_products.sh
+++ product-information/src/test/resources/insert_demo_products.sh
@@ -6,7 +6,7 @@ curl -d '{
 	"description":"Big Taste, Big Fun and Big Pizza! Erlebe unsere großartige Kombination aus außergewöhnlichen Topping & Boden mit dem Big Crunch.Entdecke die Wagner Big Pizza Supreme: luftig-locker und herzhaft-saftig, belegt mit feinstem Mozzarella, leckeren Champignons, naturgereifter Edel-Salami, echtem gekochten Schinken und sonnengereiften Tomaten. Unser Geheimrezept für diesen Supreme-Genuss lautet: guter Teig, viel Belag und unser traditionelles Original Wagner Backverfahren. So entsteht die großartige Kombination aus herzhaft-saftigem Belag und dem luftig-lockeren Boden mit dem einzigartigen Big Crunch. Diese Big Pizza von Wagner macht ihrem Namen alle Ehre; eine Wagner Pizza, die Du im Haus haben solltest, wenn der große Hunger kommt oder ein paar gute Freunde! - mit extra viel Käse - extra reichhaltig belegt - großartige Kombination aus naturgereifter Salami, saftigem Kochschinken und leckeren Champignons\r\n - luftig-lockerer Teig mit dem Big Crunch", 
 	"productNumber":"2670536",
 	"image":"25165635"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"REWE Bio Frische Bio-Eier 6 Stück",
 	"vendor":"REWE Bio",
@@ -14,7 +14,7 @@ curl -d '{
 	"description":"Hergestellt für: REWE Markt GmbH, D-50603 Köln.", 
 	"productNumber":"0197892",
 	"image":"4777810"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"REWE Beste Wahl Bananen",
 	"vendor":"REWE",
@@ -22,7 +22,7 @@ curl -d '{
 	"description":"Kl.: M; Ursprungsland und Klasse können je nach Angebot abweichen. Bananen werden grün geerntet, denn sie reifen nach. Die Büschel werden gewaschen, in Hände zerteilt und verpackt. Sie werden auf besonderen Bananenschiffen in die Verbraucherländer transportiert. Vorwiegend angebaut in Ecuador, Panama, Costa Rica, Kolumbien und Dominikanische Republik. Fleischig, aromatisch und je nach Reifegrad von leicht säuerlich bis sehr süß. Überwiegend Rohverzehr, Milchmixgetränke, aber auch Beilage zu würzigen färbt sich die Schale gelblich-grau bis schwarz und es kommt zu Aromaverlusten. Bananen sind leicht verdaulich und besonders für Kleinkinder sowie als Roh- und Diätkost geeignet. Sie liefern schnell Energie und stellen eine gute Zwischenmahlzeit auch für unterwegs dar. Die Krümmung der Banane entsteht während des Wachstums hormonbedingt.",
 	"productNumber":"1028378",
 	"image":"4985655"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"REWE Bio Fettarme H-Milch 1,5% 1l",
 	"vendor":"REWE Bio",
@@ -30,7 +30,7 @@ curl -d '{
 	"description":"Das REWE Bio Müsli schmeckt herrlich getreidig. Ideal mit Milch, Joghurt oder pur als Knabberei.\n Naturland Getreide aus kontrolliert biologischem Anbau ist die Grundlage für das REWE Bio Müsli. Natürliche Düngung, bodenschonender Anbau und strenge Kontrollen nach Naturland Richtlinien sorgen für ein Müsli der Extraklasse. Diese Standards, die weit über die gesetzlichen Anforderungen der EU-Bio-Verordnung hinausgehen, garantieren Natürlichkeit und Geschmack. Genießen Sie den Unterschied!", 
 	"productNumber":"0197877",
 	"image":"1163210"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"REWE Bio Schokomüsli 500g",
 	"vendor":"REWE",
@@ -38,7 +38,7 @@ curl -d '{
 	"description":"Das REWE Bio Müsli schmeckt herrlich getreidig. Ideal mit Milch, Joghurt oder pur als Knabberei.\n Naturland Getreide aus kontrolliert biologischem Anbau ist die Grundlage für das REWE Bio Müsli. Natürliche Düngung, bodenschonender  Anbau und strenge Kontrollen nach Naturland Richtlinien sorgen für ein Müsli der Extraklasse. Diese Standards, die weit über die gesetzlichen Anforderungen der EU-Bio-Verordnung hinausgehen, garantieren Natürlichkeit und Geschmack. Genießen Sie den Unterschied!", 
 	"productNumber":"0974662",
 	"image":"7891620"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"ja! Markenbutter 250g",
 	"vendor":"ja!",
@@ -46,7 +46,7 @@ curl -d '{
 	"description":"Deutsche Markenbutter, Mildgesäuerte Butter, 82% Fett", 
 	"productNumber":"5883121",
 	"image":"5412060"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"Van Nahmen Bio Aroniasaft 0,75l",
 	"vendor":"Van Nahmen",
@@ -54,7 +54,7 @@ curl -d '{
 	"description":"Der Aroniastrauch stamm ursprünglich aus Nordamerika. Für unseren schonend gepressten, reinen Direktsaft verwenden wir ausschließlich frische Bio-Aroniabeeren. Der herbe Aroniasaft schmeckt hervorragend als Schorle sowie mit Apfelsaft oder Tee. Er ist reich an Polyphenolen.", 
 	"productNumber":"0718568",
 	"image":"22812940"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"REWE Bio Roggenbrötchen 6 Stück",
 	"vendor":"REWE Bio",
@@ -62,7 +62,7 @@ curl -d '{
 	"description":"Hergestellt für: REWE Markt GmbH, D-50603 Köln.", 
 	"productNumber":"0321145",
 	"image":"3515050"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"Homann Feiner Nudelsalat 400g",
 	"vendor":"Homann",
@@ -70,7 +70,7 @@ curl -d '{
 	"description":"Feiner Nudelsalat mit Schinkenwurst & Ei", 
 	"productNumber":"4925190",
 	"image":"2323180"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products &&
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products &&
 curl -d '{
 	"name":"Maggi Instant Klare Gemüsebrühe 7l",
 	"vendor":"Maggi",
@@ -78,4 +78,4 @@ curl -d '{
 	"description":"Gemüsebrühe.", 
 	"productNumber":"3066450",
 	"image":"26034526"
-	}' -H "Content-Type: application/json" -X POST http://localhost:8082/products
+	}' -H "Content-Type: application/json" -X POST http://localhost:9082/products
