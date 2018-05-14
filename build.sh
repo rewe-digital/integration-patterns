e1610f7653d71789878ca9f2937c477547f2a32d
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-05-14 11:32:15 +0200

    added aggregation pom to build all projects

--- build.sh
+++ build.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 echo "Building all procjects.."
-cd composer-service
 mvn clean verify -DskipTests
-cd ..
-cd header-footer
-mvn clean verify -DskipTests
-cd ..
-cd product-detail-page
-mvn clean verify -DskipTests
-cd ..
-cd product-information
-mvn clean verify -DskipTests
-cd ..
-cd simplekafka
-mvn clean verify -DskipTests
-cd ..
